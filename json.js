cb24ba0c83d975bfc08a958ed99c92c8a897595b
tharani234 <81137960+tharani234@users.noreply.github.com>
2021-03-30 18:21:46 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,17 @@
+let user = {
+  name: "anu",
+  age: 21,
+  id:7809,
+
+  toString() {
+    return `{name: "${this.name}", age:"${this.age}",id:"${this.id}"}`;
+  }
+};
+
+console.log(user);
+console.log(user.id);
+console.log(user.name);
+console.log(user.age);
+ 
+
+ 
