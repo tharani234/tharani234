d65b3bea3e4f09b2ad9a9d1079337d0de61264b1
tharani234 <81137960+tharani234@users.noreply.github.com>
2021-04-01 15:21:49 +0530

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,11 @@
+let multi = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]];
+let multisum = 0;
+for (i = 0; i < multi.length; i++) {
+
+    multi[i].forEach(element => {
+
+        multisum = multisum + element;
+    });
+}
+console.log(multisum);
+
