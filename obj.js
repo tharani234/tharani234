8e2e5c58a0943aef89058adebd799fc7eb0fd39c
tharani234 <81137960+tharani234@users.noreply.github.com>
2021-04-01 15:13:54 +0530

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,54 @@
+var students = [["anu", "Hema", "Arun", "Pavi", "Guna"], {
+
+
+    userId: 1,
+    name: "Anu",
+    DOB: "04-07-99",
+    age: "21",
+    dept: "cse",
+    gender: "female",
+    id: 01
+},
+{
+    userId: 2,
+    name: "Hema",
+    DOB: "09-08-99",
+    age: "21",
+    dept: "ece",
+    gender: "female",
+    id: 02
+},
+{
+    userId: 3,
+    name: "Arun",
+    DOB: "11-04-99",
+    age: "21",
+    dept: "civil",
+    gender: "male",
+    id: 03
+},
+{
+    userId: 4,
+    name: "Pavi",
+    DOB: "06-07-99",
+    age: "21",
+    dept: "cse",
+    gender: "female",
+    id: 04
+},
+{
+    userId: 5,
+    name: "Guna",
+    DOB: "03-02-99",
+    age: "21",
+    dept: "mech",
+    gender: "male",
+    id: 05
+}
+];
+console.log(students[4].name);
+console.log(students[1].age);
+console.log(students[2].DOB);
+console.log(students[3].dept);
+console.log(students[1].gender);
+console.log(students[2].id);
