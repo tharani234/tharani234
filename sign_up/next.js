4a0a4d6328f9676806f41d9c8829bd57f7278099
tharani234 <81137960+tharani234@users.noreply.github.com>
2021-04-28 08:14:23 +0530

    Update next.js

--- sign_up/next.js
+++ sign_up/next.js
@@ -1,15 +1,46 @@
 function validate()
 {
-    var username=document.getElementById("uname");
-    if(username.value=="")
-    {
-        setErrorFor(username, 'Username cannot be blank');
-   return false;
-}
-var password=document.getElementById("pass");
-if(password.value=="")
+
+var name=document.myForm.first.value;
+if(name=="")
 {
-    setErrorFor(password, 'Password cannot be blank')
+    alert('enter first name');
     return false;
 }
+var name=document.myForm.last.value;
+if(name=="")
+{
+    alert('enter last name');
+    return false;
+}
+    var username=document.myForm.uname.value;
+    if(username=="")
+    {
+        alert('Username cannot be blank');
+        return false;
+    }
+var password=document.myForm.password.value;
+if(password.length<8)
+{
+    alert('Password must be at least 8 characters long');
+    return false;
+}
+var password=document.myForm.confirm.value;
+if(password.length<8)
+{
+    alert('Confirm password must be at least 8 characters long');
+    return false;
+}
+var pwd = /^(?=.*[0-9])$/;
+var password = document.myForm.password.value;
+  if(password.value.match(pwd)) {  
+    alert('check password value');
+    return false;
+  }
+var pwd = /^(?=.*[A-Z])$/;
+var password = document.myForm.password.value;
+  if(password.value.match(pwd)) {  
+    alert('check password value');
+    return false;
+  }
 }
