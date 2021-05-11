f68c68a5d69e77bf4ed96dcb6388259909e46025
tharani234 <81137960+tharani234@users.noreply.github.com>
2021-05-11 16:51:41 +0530

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<title>Variable</title>
+<body>
+<?php
+$txt = "Welcome";
+$x = 150;
+$y = 87.6;
+$z=$x-$y;
+
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y."<br>";
+echo $z."<br>";
+echo "$txt to my home";
+?>
+</body>
+</html>
