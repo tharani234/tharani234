ffbb94b72ad12329969446a56f2b199b0d56735f
tharani234 <81137960+tharani234@users.noreply.github.com>
2021-07-08 22:49:18 +0530

    Create App.js

---
+++ React/Selector/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+import Values from './components/values'
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+                   <Values />
+        </div>
+    )
+  }
+}
+
+export default App
